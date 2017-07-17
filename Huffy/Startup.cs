47592733c40a71dbb18bb73c5da6b4f958fc5c29
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-07-17 13:01:02 -0600

    Add project files.

---
+++ Huffy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Huffy.Startup))]
+namespace Huffy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
