e4cf25103a0b4384159df336ec7646e35b77d9db
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-07-18 14:13:13 -0600

    Add Basic Form

---
+++ Huffy/Controllers/StabsController.cs
@@ -0,0 +1,28 @@
+﻿using Huffy.Models;
+using Huffy.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Huffy.Controllers
+{
+    public class StabsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public StabsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new StabFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Huffy/ViewModels/StabFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Huffy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Huffy.ViewModels
+{
+    public class StabFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
