468c286a35bd22ecbae38414c8993ae64d083405
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-07-19 09:10:56 -0600

    Saving Data

--- Huffy/Controllers/StabsController.cs
+++ Huffy/Controllers/StabsController.cs
@@ -1,5 +1,6 @@
 ﻿using Huffy.Models;
 using Huffy.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,7 @@ namespace Huffy.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new StabFormViewModel
@@ -24,5 +26,26 @@ namespace Huffy.Controllers
             };
             return View(viewModel);
         }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(StabFormViewModel viewmodel)
+        {
+            var userId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == userId);
+            var gener = _context.Genres.Single(g => g.Id == viewmodel.Genre);
+            var stab = new Stab
+            {
+                Venue = viewmodel.Venue,
+                DateTime = DateTime.Parse(string.Format("{0}, {1}", viewmodel.Date, viewmodel.Time)),
+                Artist = artist,
+                Genre = gener,
+            };
+            _context.Stabs.Add(stab);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
