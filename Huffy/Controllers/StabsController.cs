770257141e298452e77dc9ef17f97f73851a90cf
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-07-28 14:48:46 -0600

    Edit, Update, Mine Controler

--- Huffy/Controllers/StabsController.cs
+++ Huffy/Controllers/StabsController.cs
@@ -3,6 +3,7 @@ using Huffy.ViewModels;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,26 +19,40 @@ namespace Huffy.Controllers
         }
 
         [Authorize]
+        public ActionResult Mine()
+        {
+            var stab = _context.Stabs
+                .Include(global => global.Genre)
+                .ToList();
+            return View(stab);
+        }
+
+
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new StabFormViewModel
             {
                 Genres = _context.Genres.ToList()
             };
-            return View(viewModel);
+            return View("Create", viewModel);
         }
 
         [HttpPost]
         [Authorize]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(StabFormViewModel viewmodel)
         {
-            //var userId = User.Identity.GetUserId();
-            //var artist = _context.Users.Single(u => u.Id == userId);
-            //var gener = _context.Genres.Single(g => g.Id == viewmodel.Genre);
+            if (!ModelState.IsValid)
+            {
+                viewmodel.Genres = _context.Genres.ToList();
+                return View("create", viewmodel);
+            }
+            
             var stab = new Stab
             {
                 Venue = viewmodel.Venue,
-                DateTime = viewmodel.DateTime,
+                DateTime = viewmodel.GetDateTime(),
                 ArtistId = User.Identity.GetUserId(),
                 GenreId = viewmodel.Genre,
             };
@@ -46,6 +61,50 @@ namespace Huffy.Controllers
 
             return RedirectToAction("Index", "Home");
         }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var stab = _context.Stabs.SingleOrDefault(u => u.Id == id && u.ArtistId == userId);
+
+            var viewModel = new StabFormViewModel
+            {
+                Date = stab.DateTime.ToString("d MMM yyyy"),
+                Time = stab.DateTime.ToString("HH:mm"),
+                Genre = stab.GenreId,
+                Venue = stab.Venue,
+                Genres = _context.Genres.ToList(),
+                Id = stab.Id,
+                Heading = "Edit",
+            };
+
+            return RedirectToAction("Mine", "Stabs");
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(StabFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            }
+
+            var userId = User.Identity.GetUserId();
+            var stab = _context.Stabs.SingleOrDefault(u => u.Id == viewModel.Id && u.ArtistId == userId);
+            stab.Venue = viewModel.Venue;
+            stab.DateTime = viewModel.GetDateTime();
+            stab.GenreId = viewModel.Genre;
+
+            _context.Stabs.Add(stab);
+            _context.SaveChanges();
+            return RedirectToAction("Mine", "Stabs");
+        }
+
+
 
     }
 }
--- Huffy/ViewModels/StabFormViewModel.cs
+++ Huffy/ViewModels/StabFormViewModel.cs
@@ -1,24 +1,54 @@
 ﻿using Huffy.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using GigBug.ViewModels;
+using System.Linq.Expressions;
+using Huffy.Controllers;
+using System.Web.Mvc;
 
 namespace Huffy.ViewModels
 {
     public class StabFormViewModel
     {
+        public int Id { get; set; }
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime
+
+        public string Heading { get; set; }
+
+        //public string Action
+        //{
+        //    get
+        //    {
+        //        Expression<Func<StabsController, ActionResult>> update =
+        //            (c => c.Update(this));
+
+        //        Expression<Func<StabsController, ActionResult>> create =
+        //            (c => c.Create(this));
+
+        //        var action = (Id != 0) ? update : create;
+        //        return (action.Body as MethodCallExpression).Method.Name;
+        //    }
+        //}
+
+        public DateTime GetDateTime()
         {
-            get
-            {
                 return DateTime.Parse(string.Format("{0}, {1}", Date, Time));
-            }
         }
     }
 }
